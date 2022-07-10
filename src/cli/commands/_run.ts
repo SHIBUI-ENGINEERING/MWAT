6eac2f43557bbb3569cd80f40a19e57ce155478a
Vsevolod Pletnev <mr.sevapp@gmail.com>
2022-07-11 00:08:29 +0400

    .

--- src/cli/commands/_run.ts
+++ src/cli/commands/_run.ts
@@ -1,8 +1,20 @@
 import { resolve } from "https://deno.land/std@0.147.0/path/mod.ts";
+import { STR_CLI_HELP_RUN } from "../../_strings.ts";
+
+function validate(args: string[]): boolean {
+  return true;
+}
 
 export async function run(args: string[]) {
+  if (!validate(args)) {
+    console.log(STR_CLI_HELP_RUN());
+  }
+
   const relatiovePath = args[0];
   const absolutePath = resolve(Deno.cwd(), relatiovePath);
+
+  console.log(relatiovePath, absolutePath);
+
   const workflowModule = await import(absolutePath);
 
   console.log(workflowModule);
